edf5b5c8e1730cb82b267b377c331ba4bb6bd197
asou safari <asousafari1990@gmail.com>
2024-08-29 00:34:15 +0330

    Add Transaction model

---
+++ charging_payment/charging/src/main/java/com/company/charging/api/model/Transaction.java
@@ -0,0 +1,10 @@
+package com.company.charging.api.model;
+
+/**
+ * Author: ASOU SAFARI
+ * Date:8/29/24
+ * Time:12:24 AM
+ */
+public class Transaction {
+
+}
---
+++ charging_payment/charging/src/main/java/com/company/charging/api/model/TransactionType.java
@@ -0,0 +1,8 @@
+package com.company.charging.api.model;
+
+public enum TransactionType {
+    CHARGE("CHARGE"),REFUND("REFUND");
+
+    TransactionType(String name) {
+    }
+}
