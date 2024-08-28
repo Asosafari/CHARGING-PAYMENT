ee6a52ac676f33787b65b317c54d3fb30f411375
asou safari <asousafari1990@gmail.com>
2024-08-29 00:31:27 +0330

    Add ChargingPlan abstract

---
+++ charging_payment/charging/src/main/java/com/company/charging/api/model/ChargingPlan.java
@@ -0,0 +1,56 @@
+package com.company.charging.api.model;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Author: ASOU SAFARI
+ * Date:8/29/24
+ * Time:12:17 AM
+ */
+
+@Entity
+@Data
+@SuperBuilder
+@Table(name = "charging_plan")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class ChargingPlan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "plan_name",unique = true, nullable = false)
+    private String planName;
+
+    @Column(name = "rate_per_unit",nullable = false)
+    private BigDecimal ratePerUnit;
+
+
+    @Column(name = "description")
+    private String description;
+
+    @Builder.Default
+    @ManyToMany(mappedBy = "chargingPlans")
+    private Set<User> users = new HashSet<>();
+
+    @Version
+    private Integer version;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    private LocalDateTime updateDate;
+
+}
--- charging_payment/payment/src/main/java/com/bank/payment/PaymentApplication.java
+++ charging_payment/payment/src/main/java/com/bank/payment/PaymentApplication.java
@@ -1,4 +1,4 @@
-package com.company.payment;
+package com.bank.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
