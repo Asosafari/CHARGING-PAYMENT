0be0822b3bee34754f3936e5c5da6a5e0659ca42
asou safari <asousafari1990@gmail.com>
2024-08-29 23:38:11 +0330

    Add UserMapper

---
+++ charging_payment/charging/src/main/java/com/company/charging/api/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.company.charging.api.mapper;
+
+import com.company.charging.api.dto.UserDTO;
+import com.company.charging.api.model.User;
+import org.mapstruct.Mapper;
+
+/**
+ * Author: ASOU SAFARI
+ * Date:8/29/24
+ * Time:11:04 PM
+ */
+@Mapper
+public interface UserMapper {
+    User mapUserDTO(UserDTO userDTO);
+    UserDTO mapUser(User user);
+}
+
