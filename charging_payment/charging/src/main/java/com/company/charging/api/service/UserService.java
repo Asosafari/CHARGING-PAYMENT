316398da190496cdd1af9a54b952007873ec7670
asou safari <asousafari1990@gmail.com>
2024-08-29 23:39:42 +0330

    implement UserService

---
+++ charging_payment/charging/src/main/java/com/company/charging/api/service/UserService.java
@@ -0,0 +1,23 @@
+package com.company.charging.api.service;
+
+import com.company.charging.api.dto.UserDTO;
+import com.company.charging.api.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * Author: ASOU SAFARI
+ * Date:8/29/24
+ * Time:10:39 PM
+ */
+@Service
+public interface UserService {
+    Page<UserDTO> listOfUsers(Integer pageNumber, Integer pageSize);
+    Optional<UserDTO> getUserById(Long id);
+    Optional<UserDTO> updateUser(Long id, UserDTO userDto);
+    boolean deleteUser(Long id);
+
+
+}
--- charging_payment/charging/src/main/java/com/company/charging/api/service/UserServiceImpl.java
+++ charging_payment/charging/src/main/java/com/company/charging/api/service/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package com.company.charging.api.service;
 
 import com.company.charging.api.dto.UserDTO;
+import com.company.charging.api.mapper.UserMapper;
+import com.company.charging.api.model.User;
+import com.company.charging.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Author: ASOU SAFARI
@@ -12,29 +20,43 @@ import java.util.Optional;
  * Time:10:52 PM
  */
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
     @Override
     public Page<UserDTO> listOfUsers(Integer pageNumber, Integer pageSize) {
-        return null;
+        PageRequest pageRequest = BuildPageRequest.build(pageNumber,pageSize,"username");
+        return userRepository.findAll(pageRequest).map(userMapper ::mapUser);
     }
 
     @Override
-    public Optional<UserDTO> getUserById(Long Id) {
-        return Optional.empty();
+    public Optional<UserDTO> getUserById(Long id) {
+        return Optional.ofNullable(userMapper.mapUser(userRepository.findById(id).orElse(null)));
     }
 
     @Override
-    public Optional<UserDTO> updateUser(Long Id, UserDTO userDto) {
-        return Optional.empty();
+    public Optional<UserDTO> updateUser(Long id, UserDTO userDto) {
+        AtomicReference<Optional<UserDTO>> atomicReference = new AtomicReference<>();
+        userRepository.findById(id).ifPresentOrElse(founduser -> {
+            founduser.setEmail(userDto.getEmail());
+            founduser.setPassword(userDto.getPassword());
+            founduser.setUpdateDate(LocalDateTime.now());
+            atomicReference.set(Optional.of(userMapper.mapUser(userRepository.save(founduser))));
+        }, () -> atomicReference.set(Optional.empty())
+        );
+        return atomicReference.get();
     }
 
     @Override
     public boolean deleteUser(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()){
+            user.get().softDelete();
+            userRepository.save(user.get());
+            return true;
+        }
         return false;
-    }
-
-    @Override
-    public Optional<UserDTO> patchUser(Long Id, UserDTO userDto) {
-        return Optional.empty();
     }
 }
