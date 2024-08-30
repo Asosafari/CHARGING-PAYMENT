84e3cb8db0ba25dbc6b2d82ea55a9909713e1240
asou safari <asousafari1990@gmail.com>
2024-08-31 01:51:33 +0330

    Migration

---
+++ charging_payment/charging/src/main/resources/db/migration/V1__init-mysql-database.sql
@@ -0,0 +1,107 @@
+
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(12) NOT NULL UNIQUE,
+    password VARCHAR(12) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    balance DECIMAL(19, 4) NOT NULL CHECK (balance >= 0),
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_date TIMESTAMP,
+    version INT NOT NULL DEFAULT 1,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE
+)engine=InnoDB;
+
+CREATE TABLE charging_plans (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    plan_name VARCHAR(255) NOT NULL UNIQUE,
+    rate_per_unit DECIMAL(19, 4) NOT NULL,
+    price_per_unit DECIMAL(19, 4) NOT NULL,
+    description TEXT,
+    version INT NOT NULL DEFAULT 1,
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE
+)engine=InnoDB;
+
+
+CREATE TABLE transactions (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    charging_plan_id BIGINT NOT NULL,
+    amount DECIMAL(19, 4) NOT NULL CHECK (amount >= 0),
+    is_success BOOLEAN NOT NULL,
+    transaction_type ENUM('DIRECT', 'GATEWAY') NOT NULL,
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (charging_plan_id) REFERENCES charging_plans(id)
+)engine=InnoDB;
+
+
+CREATE TABLE user_charging_plan (
+    user_id BIGINT NOT NULL,
+    charging_plan_id BIGINT NOT NULL,
+    version INT NOT NULL DEFAULT 1,
+    PRIMARY KEY (user_id, charging_plan_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (charging_plan_id) REFERENCES charging_plans(id)
+)engine=InnoDB;
+
+
+CREATE TABLE authorized_bank_users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    bank_user_id BIGINT NOT NULL UNIQUE,
+    token VARCHAR(255) NOT NULL UNIQUE,
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)engine=InnoDB;
+
+
+CREATE TABLE successful_transactions (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    transaction_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    token VARCHAR(255) NOT NULL,
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (transaction_id) REFERENCES transactions(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)engine=InnoDB;
+CREATE TABLE logging_event (
+  event_id BIGINT NOT NULL AUTO_INCREMENT,
+  timestmp BIGINT NOT NULL,
+  formatted_message TEXT NOT NULL,
+  logger_name VARCHAR(255) NOT NULL,
+  level_string VARCHAR(255) NOT NULL,
+  thread_name VARCHAR(255),
+  reference_flag SMALLINT,
+  caller_filename VARCHAR(255) NOT NULL,
+  caller_class VARCHAR(255) NOT NULL,
+  caller_method VARCHAR(255) NOT NULL,
+  caller_line CHAR(4) NOT NULL,
+  arg0 VARCHAR(255),
+  arg1 VARCHAR(255),
+  arg2 VARCHAR(255),
+  arg3 VARCHAR(255),
+  PRIMARY KEY (event_id)
+);
+
+CREATE TABLE logging_event_property (
+  event_id BIGINT NOT NULL,
+  mapped_key VARCHAR(255) NOT NULL,
+  mapped_value TEXT,
+  PRIMARY KEY (event_id, mapped_key),
+  FOREIGN KEY (event_id) REFERENCES logging_event(event_id)
+);
+
+CREATE TABLE logging_event_exception (
+  event_id BIGINT NOT NULL,
+  i SMALLINT NOT NULL,
+  trace_line VARCHAR(255) NOT NULL,
+  PRIMARY KEY (event_id, i),
+  FOREIGN KEY (event_id) REFERENCES logging_event(event_id)
+);
+
+
+
+
---
+++ charging_payment/charging/src/main/resources/db/migration/V2__init-mysql-database.sql
@@ -0,0 +1,57 @@
+
+CREATE VIEW transaction_view AS
+SELECT t.id AS transaction_id, t.user_id, u.username, t.charging_plan_id, cp.plan_name, t.amount, t.is_success, t.created_date
+FROM transactions t
+JOIN users u ON t.user_id = u.id
+JOIN charging_plans cp ON t.charging_plan_id = cp.id;
+
+
+CREATE VIEW successful_transaction_view AS
+SELECT st.id AS successful_transaction_id, st.transaction_id, st.user_id, u.username, st.token, st.created_date
+FROM successful_transactions st
+JOIN users u ON st.user_id = u.id;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE get_authorized_bank_users(
+    IN p_user_id BIGINT,
+    OUT p_token VARCHAR(255)
+)
+BEGIN
+    DECLARE v_token VARCHAR(255);
+    SELECT token INTO v_token
+    FROM authorized_bank_users
+    WHERE user_id = p_user_id;
+
+
+    IF v_token IS NOT NULL THEN
+        SET p_token = v_token;
+    ELSE
+        SET p_token = '"UNAUTHORIZED"';
+    END IF;
+END$$
+
+DELIMITER ;
+DELIMITER $$
+
+CREATE PROCEDURE charging_account(
+    IN p_user_id BIGINT,
+    IN p_rate_per_unit DECIMAL(19, 4),
+    OUT p_current_balance DECIMAL(19, 4)
+)
+BEGIN
+    DECLARE v_amount_to_add DECIMAL(19, 4);
+
+    SET v_amount_to_add = p_rate_per_unit * 1000;
+
+    UPDATE users
+    SET balance = balance + v_amount_to_add
+    WHERE id = p_user_id;
+
+    SELECT balance INTO p_current_balance
+    FROM users
+    WHERE id = p_user_id;
+END$$
+
+DELIMITER ;
