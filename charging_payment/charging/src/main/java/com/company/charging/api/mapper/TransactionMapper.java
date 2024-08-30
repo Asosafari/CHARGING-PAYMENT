ac04fd919ae431d680e9de1e04d00c24abc83fa0
asou safari <asousafari1990@gmail.com>
2024-08-31 00:15:45 +0330

    fix: Add if condition to mapper  TransactionMapper

--- charging_payment/charging/src/main/java/com/company/charging/api/mapper/TransactionMapper.java
+++ charging_payment/charging/src/main/java/com/company/charging/api/mapper/TransactionMapper.java
@@ -3,10 +3,18 @@ package com.company.charging.api.mapper;
 import com.company.charging.api.dto.TransactionDTO;
 import com.company.charging.api.model.Transaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface TransactionMapper {
-    default TransactionDTO mapToDto(Transaction transaction){
+
+    TransactionMapper INSTANCE = Mappers.getMapper(TransactionMapper.class);
+    default TransactionDTO mapToDto(Transaction transaction) {
+        if (transaction == null) {
+            return null;
+        }
+
+
         return TransactionDTO.builder()
                 .id(transaction.getId())
                 .amount(transaction.getAmount())
