3154ce54be4419a940986b5ead333197a0570784
asou safari <asousafari1990@gmail.com>
2024-08-31 20:04:57 +0330

    edit: ChargingPlanDTO

--- charging_payment/charging/src/main/java/com/company/charging/api/dto/ChargingPlanDTO.java
+++ charging_payment/charging/src/main/java/com/company/charging/api/dto/ChargingPlanDTO.java
@@ -1,4 +1,6 @@
 package com.company.charging.api.dto;
+import com.company.charging.api.model.ChargingPlanType;
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -9,13 +11,13 @@ import java.math.BigDecimal;
  * Time:1:21 AM
  */
 @Data
+@Builder
 public class ChargingPlanDTO {
-
     private Long id;
     private String planName;
     private BigDecimal ratePerUnit;
     private BigDecimal pricePerUnit;
     private String description;
-
+    private ChargingPlanType chargingPlanType;
 
 }
