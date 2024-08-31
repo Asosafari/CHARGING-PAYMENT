cee6c9d3260179d68264a4e7e7ac347d64ec11ef
asou safari <asousafari1990@gmail.com>
2024-08-31 20:55:38 +0330

    feat: add CreateChargingPlan ChargingService

--- charging_payment/charging/src/main/java/com/company/charging/api/service/ChargingService.java
+++ charging_payment/charging/src/main/java/com/company/charging/api/service/ChargingService.java
@@ -1,6 +1,7 @@
 package com.company.charging.api.service;
 
 import com.company.charging.api.dto.ChargingPlanDTO;
+import com.company.charging.api.model.ChargingPlan;
 import org.springframework.data.domain.Page;
 
 import java.math.BigDecimal;
@@ -9,7 +10,8 @@ import java.util.Optional;
 public interface ChargingService {
     Page<ChargingPlanDTO>listOfChargingPlan(BigDecimal greaterThanRate,BigDecimal lessThanPricePerUnit,
                                       Integer pageNumber, Integer pageSize,String sortProperty);
-    Optional<ChargingPlanDTO> getplanById(Long id);
+    Optional<ChargingPlanDTO> getPlanById(Long id);
+    ChargingPlanDTO CreateChargingPlan(ChargingPlanDTO chargingPlanDTO);
     Optional<ChargingPlanDTO> updatePlan(Long id, ChargingPlanDTO chargingPlanDTO);
     boolean deletePlan(Long id);
 
