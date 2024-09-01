6fa2907c2f7d0e712773129094f6b54ed9c69e71
asou safari <asousafari1990@gmail.com>
2024-09-01 17:57:35 +0330

    feat: Add new Method mapToModel

--- charging_payment/charging/src/main/java/com/company/charging/api/mapper/TransactionMapper.java
+++ charging_payment/charging/src/main/java/com/company/charging/api/mapper/TransactionMapper.java
@@ -2,27 +2,42 @@ package com.company.charging.api.mapper;
 
 import com.company.charging.api.dto.TransactionDTO;
 import com.company.charging.api.model.Transaction;
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 
-@Mapper
-public interface TransactionMapper {
+import java.time.LocalDateTime;
 
-    TransactionMapper INSTANCE = Mappers.getMapper(TransactionMapper.class);
-    default TransactionDTO mapToDto(Transaction transaction) {
+@Component
+public class TransactionMapper {
+
+    public TransactionDTO mapToDto(Transaction transaction) {
         if (transaction == null) {
             return null;
         }
-
-
         return TransactionDTO.builder()
                 .id(transaction.getId())
                 .amount(transaction.getAmount())
                 .chargingPlanName(transaction.getChargingPlan().getPlanName())
+                .chargingPlanId(transaction.getChargingPlan().getId())
                 .transactionType(transaction.getTransactionType())
                 .isDeleted(transaction.isDeleted())
                 .username(transaction.getUser().getUsername())
+                .userId(transaction.getUser().getId())
                 .isSuccess(transaction.isSuccess())
+                .build();
+    }
+
+    public Transaction mapToModel(TransactionDTO transactionDTO){
+        if (transactionDTO == null){
+            return null;
+        }
+       return Transaction.builder()
+                .user(transactionDTO.getUser())
+                .chargingPlan(transactionDTO.getChargingPlan())
+                .description(transactionDTO.getDescription())
+                .createdDate(LocalDateTime.now())
+                .amount(transactionDTO.getAmount())
+                .isSuccess(transactionDTO.isSuccess())
+                .isDeleted(transactionDTO.isDeleted())
                 .build();
     }
 }
