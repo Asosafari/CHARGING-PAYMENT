54d9d33469618fb0b55302826a4e8b7825a3867f
asou safari <asousafari1990@gmail.com>
2024-09-01 17:56:10 +0330

    feat: Add new field to TransactionDTO -> to transport more flexible

--- charging_payment/charging/src/main/java/com/company/charging/api/dto/TransactionDTO.java
+++ charging_payment/charging/src/main/java/com/company/charging/api/dto/TransactionDTO.java
@@ -1,6 +1,9 @@
 package com.company.charging.api.dto;
 
+import com.company.charging.api.model.ChargingPlan;
 import com.company.charging.api.model.TransactionType;
+import com.company.charging.api.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Data;
 
@@ -19,10 +22,18 @@ public class TransactionDTO {
     private Long id;
     private String chargingPlanName;
     private String username;
+    private Long userId;
+    private Long chargingPlanId;
     private BigDecimal amount;
     private boolean isSuccess;
     private boolean isDeleted;
     private TransactionType transactionType;
+    private String publicKey;
+    private String description;
+    @JsonIgnore
+    private User user;
+    @JsonIgnore
+    private ChargingPlan chargingPlan;
 
 
 }
