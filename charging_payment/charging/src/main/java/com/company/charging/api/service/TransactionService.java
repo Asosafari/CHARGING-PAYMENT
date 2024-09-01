4d6feb76c0febf8ad1692dee339f3e794dca1053
asou safari <asousafari1990@gmail.com>
2024-09-01 17:58:50 +0330

    feat: change method parameters TransactionService

--- charging_payment/charging/src/main/java/com/company/charging/api/service/TransactionService.java
+++ charging_payment/charging/src/main/java/com/company/charging/api/service/TransactionService.java
@@ -1,16 +1,16 @@
 package com.company.charging.api.service;
 
 import com.company.charging.api.dto.TransactionDTO;
-import com.company.charging.api.model.Order;
+import com.company.charging.api.dto.OrderDTO;
 import org.springframework.data.domain.Page;
 import java.util.Optional;
 
 public interface TransactionService {
-    Optional<TransactionDTO> createTrasaction(Order order);
+    Optional<TransactionDTO> createTrasaction(OrderDTO orderDTO);
     Page<TransactionDTO> getAllTransacton(String username,
                                           String chargingPlanName,
                                           Integer pageNumber,
                                           Integer pageSize);
 
-    public boolean deleteTrasction(Long id);
+    boolean deleteTrasction(Long id);
 }
