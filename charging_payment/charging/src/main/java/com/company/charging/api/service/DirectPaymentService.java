b450cea9d7b0218195161f28642d630547ba0d1d
asou safari <asousafari1990@gmail.com>
2024-09-02 12:01:43 +0330

    feat: ADD savePublicKey

--- charging_payment/charging/src/main/java/com/company/charging/api/service/DirectPaymentService.java
+++ charging_payment/charging/src/main/java/com/company/charging/api/service/DirectPaymentService.java
@@ -1,10 +1,15 @@
 package com.company.charging.api.service;
 import com.company.charging.api.dto.TransactionDTO;
-import com.company.charging.api.mapper.TransactionMapper;
+import com.company.charging.api.dto.UserDTO;
 import com.company.charging.api.dto.DirectPaymentRequest;
 import com.company.charging.api.repository.TransactionRepository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,16 +22,20 @@ import org.springframework.web.client.RestTemplate;
  */
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DirectPaymentService {
     private final TransactionRepository transactionRepository;
 
+    @PersistenceContext
+    private final EntityManager entityManager;
+
     private final RestTemplate restTemplate = new RestTemplate();
-    private final TransactionMapper transactionMapper;
 
     public boolean processPayment(TransactionDTO transactionDTO) {
         DirectPaymentRequest directPaymentRequest = createPaymentRequest(transactionDTO);
+        directPaymentRequest.setEncryptedData(directPaymentRequest.getUserId() + "|" + directPaymentRequest.getAmount());
 
-        String paymentApiUrl = "http://localhost:9000/api/v1/payments";
+        String paymentApiUrl = "http://localhost:9090/api/v1/payments";
         ResponseEntity<String> response = restTemplate.postForEntity(paymentApiUrl, directPaymentRequest, String.class);
 
         if (response.getStatusCode() == HttpStatus.OK) {
@@ -39,14 +48,46 @@ public class DirectPaymentService {
     }
 
 
+    @Transactional
+    public boolean registerToPaymentSystem(UserDTO userDTO){
+        try {
+            String paymentApiUrl = "http://localhost:9090/api/v1/paymentUsers/regester";
+            ResponseEntity<String> response = restTemplate.postForEntity(paymentApiUrl,userDTO,String.class);
+            if (response.getStatusCode() == HttpStatus.CREATED){
+                savePublicKey(userDTO.getId(),userDTO.getId(),response.getBody());
+                return true;
+            }
+            log.error("UNAUTHORIZED REQUEST");
+            return false;
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return false;
+        }
+
+    }
+
+
     private DirectPaymentRequest createPaymentRequest(TransactionDTO transactionDTO) {
         return DirectPaymentRequest.builder()
-                .userId(transactionDTO.getUserId())
-                .chargingPlanId(transactionDTO.getChargingPlanId())
                 .amount(transactionDTO.getAmount())
                 .publicKey(transactionDTO.getPublicKey())
-                .transactionType(transactionDTO.getTransactionType())
+                .userId(transactionDTO.getUserId())
+                .paymentType(PaymentType.DIRECT)
                 .build();
     }
+
+
+    @Transactional
+    private int savePublicKey(Long userId, Long bankUserId, String publicKey) {
+
+        String sql = "INSERT INTO authorized_bank_users (user_id, bank_user_id, encrypted_public_key) " +
+                "VALUES (:userId, :bankUserId, :publicKey)";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("userId", userId);
+        query.setParameter("bankUserId", bankUserId);
+        query.setParameter("publicKey", publicKey);
+        return query.executeUpdate();
+    }
+
 
 }
